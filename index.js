23ac632f77d06d0e4c90cbf0ea0bb98116abce74
manugomez1206 <manugomezgallego26@gmail.com>
2025-04-28 17:12:14 -0500

    En index.js pusimos a correr la pagina por el puerto 3000

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.listen(3000, () => {
+    console.log('Server is runnig on port 3000');
+});
+
 app.get('/',(req,res) => {
     res.send('Hello World');
 });
